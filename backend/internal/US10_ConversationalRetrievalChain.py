e2e8259e5c1cf070be873a3e030905de8272102d
Emmaliyt <w243809891@qq.com>
2024-02-11 12:20:13 +0100

    update ConversationalRetrieverchain

--- backend/internal/US10_ConversationalRetrievalChain.py
+++ backend/internal/US10_ConversationalRetrievalChain.py
@@ -4,15 +4,9 @@ from langchain.chains import ConversationalRetrievalChain
 from US10_initialLLM import initial
 from US10_retriever import db
 
-def interactiveBot(query):
+def interactiveBot(query,retriever):
     
     llm, memory =initial()
-    
-    # Ordnerpfad zum Hochladen
-    folder = "../interaktion"
-    folder_path = glob.glob(os.path.join(folder, "*"))
-    
-    retriever = db(folder_path)
 
     bot = ConversationalRetrievalChain.from_llm(
         llm, retriever, memory=memory,verbose=True
--- backend/internal/US10_interaktiontemplate.py
+++ backend/internal/US10_interaktiontemplate.py
@@ -1,19 +1,15 @@
 
 from langchain.prompts.prompt import PromptTemplate
-def interaktion_prompt(sachverhalt, original_text, user_query):
+def interaktion_prompt(user_query):
     prompt_template = PromptTemplate.from_template(
         """ Du bist Sachbearbeiter und Verfasser eines juristischen Bescheids.
             Anhand einen Sachverhalt wird ein originaler Bescheid wie folgendes generiert. 
-            sachverhalt : {sac}
-            originaler Bescheid : {original}
             Auf Basis von Fragen oder Kommentare von den Benutzer solltest du diesen Bescheid erneut verfassen.
             Änderungswünsche vom Benutzer : {query}
             Der neue Text : 
         """ 
     )
     messages = prompt_template.format(
-        sac = sachverhalt,
-        original = original_text,
         query = user_query
         )
     return messages
