bb9a06381fc54831b756803cbba36aaeb2b1bc0d
comdyax <cody24@tuta.io>
2024-02-22 21:21:21 +0100

    pylint the last

--- backend/internal/utils.py
+++ backend/internal/utils.py
@@ -1,12 +1,23 @@
+""" 
+    in this module are util functions
+"""
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def check_environment():
+    """
+    checks if the environemt variables are correct
+
+    Raises:
+        ValueError: _description_
+        ValueError: _description_
+    """
     api_key = os.getenv("AZURE_OPENAI_KEY")
     if not api_key:
         raise ValueError("< API Key > nicht gefunden!")
     azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
     if not azure_endpoint:
         raise ValueError("< API Endpoint > nicht gefunden!")
+    
